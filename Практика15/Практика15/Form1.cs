575c8412df4cc645f6e0907e9492f66531bed3a8
Mrcreativschik Mrlowyaw <65216239+MrcreativschikMrlowyaw@users.noreply.github.com>
2020-05-12 15:18:02 +0300

    Практика 15

---
+++ Практика15/Практика15/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Практика15
+{
+    public partial class Form1 : Form
+    {
+        string name = "мир";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "") name = textBox1.Text;
+            textBox2.Text = ("Привет, " + name + "!");
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Практика15/Практика15_3/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Практика15_3
+{
+    public partial class Form1 : Form
+    {
+        byte i=1, b, u=0;
+        static Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            b = Convert.ToByte(textBox1.Text);
+            if (rnd.Next(10) == b)
+            { 
+                label2.Text = "Угадали!";
+                u++;
+            }
+            else label2.Text = "Не угадали!";
+            i++;
+            if (i==10)
+            {
+                label3.Text = "Коэффициент удачи: " + ((u / 10f) * 100) + "%";
+                i = 0;
+                u = 0;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
